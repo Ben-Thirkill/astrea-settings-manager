425a0e92a782b4cfbf689c3beba389133bb89e36
Ben-Thirkill <ben.t.thirkill@gmail.com>
2025-10-22 21:01:28 +0100

    feat(astrea-settings): Created function to ensure settings are loaded.

--- frontend/astrea-workspace/projects/astrea-settings/src/lib/services/settings.service.ts
+++ frontend/astrea-workspace/projects/astrea-settings/src/lib/services/settings.service.ts
@@ -1,19 +1,42 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {firstValueFrom, Observable} from 'rxjs';
 import { Setting } from '../models/setting.model';
 
 @Injectable({
-  providedIn: 'root' 
+  providedIn: 'root'
 })
 export class SettingService {
-    private _settings = new Map<string, Setting>();
+    private _settings: Map<string, Setting> | null = null;
+    private _loadingPromise?: Promise<void> | null;
 
     private http = inject(HttpClient);
-    private apiUrl = 'http://localhost:5062/api/Settings/GetAll'; 
+    private apiUrl = 'http://localhost:5062/api/Settings/GetAll';
 
     getAllSettings(): Observable<Map<string, Setting>> {
         return this.http.get<Map<string, Setting>>(this.apiUrl);
     }
 
+    public getSettingInfo(id: string) {
+        if (this._settings != null) {
+            return this._settings.get(id)
+        }
+
+        return undefined;
+    }
+
+    public async ensureLoaded(): Promise<void> {
+      if (this._settings !== null) return;
+      if (this._loadingPromise) return this._loadingPromise;
+
+      this._loadingPromise = firstValueFrom(
+          this.getAllSettings()
+      ).then(result => {
+          this._settings = new Map(Object.entries(result));
+      }).finally(() => {
+          this._loadingPromise = null;
+      });
+
+      return this._loadingPromise;
+    }
 }
