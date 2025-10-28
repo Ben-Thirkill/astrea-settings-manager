229a7ba9778b9a42764c119ac1b3b53ccdaf0c05
Ben-Thirkill <ben.t.thirkill@gmail.com>
2025-10-28 21:21:55 +0000

    feat(astrea-settings): Added settingid and clientapikey structs

---
+++ api/Core/Settings/Models/ClientApiKeyStruct.cs
@@ -0,0 +1,6 @@
+﻿namespace api.Core.Models;
+
+public readonly record struct ClientApiKey(string Value)
+{
+    public override string ToString() => Value;
+}
---
+++ api/Core/Settings/Models/SettingIdStruct.cs
@@ -0,0 +1,6 @@
+﻿namespace api.Core.Models;
+
+public readonly record struct SettingId(string Value)
+{
+    public override string ToString() => Value;
+}
